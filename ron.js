6c05273c61697f65640807319c3291ddd9067d4a
Catherine Slaughter <55809063+catieslaughts@users.noreply.github.com>
2024-07-16 12:43:41 -0500

    Create ron.js

---
+++ ron.js
@@ -0,0 +1,6 @@
+var quotes = ["quote 1", "quote 2", "quote 3"];
+
+function newQuote() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
