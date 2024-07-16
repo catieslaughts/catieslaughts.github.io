88e02605d5934c69db5fc480fd3105e33499c7b8
Catherine Slaughter <55809063+catieslaughts@users.noreply.github.com>
2024-07-16 17:45:36 -0500

    Update ron.js

--- ron.js
+++ ron.js
@@ -4,5 +4,5 @@ function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotes.length));
   var fileName = quotes[randomNumber]
   var info = '<object type="text/html" data="'.concat(fileName,'" ></object>');
-  document.getElementById('quoteDisplay').innerHTML = info;
+  document.getElementById('quoteDisplay').innerHTML = '<object type="text/html" data="quotes/billnye.html" ></object>';
 }
