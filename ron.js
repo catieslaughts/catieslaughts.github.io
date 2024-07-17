0f79848c231c1b570036a7e4950e52beeb7f8bc7
Catherine Slaughter <55809063+catieslaughts@users.noreply.github.com>
2024-07-16 19:41:44 -0500

    Update ron.js

--- ron.js
+++ ron.js
@@ -1,8 +1,11 @@
-var quotes = ["quotes/billnye_oneline.html"];
+var quotes = ["quotes/billnye_oneline.txt"];
 
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotes.length));
   var fileName = quotes[randomNumber]
-  var info = '<object type="text/html" data="'.concat(fileName,'" ></object>');
-  document.getElementById('quoteDisplay').innerHTML = '<p align="center"><font size="+1"><b>"When we explore the cosmos, we come to believe and prove that we can solve problems that have never been solved. It brings out the best in us."</b></font></p><p align="right"><font size="+1"><b>-Bill Nye</b></font></p>';
+  
+  fetch("quotes/billnye_oneline.txt").then((res) => res.text()).then((text) => {
+    var quote = text
+  }).catch((e) => console.error(e));
+  document.getElementById('quoteDisplay').innerHTML = quote
 }
