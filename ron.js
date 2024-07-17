4434f09deb1e58ae56b1a8f6f7ab30fd3d254ff3
Catherine Slaughter <55809063+catieslaughts@users.noreply.github.com>
2024-07-16 20:04:37 -0500

    Update ron.js

--- ron.js
+++ ron.js
@@ -1,10 +1,10 @@
 var quotes = [
-  ""
+  '<p align="center"><font size="+1"><b>"When we explore the cosmos, we come to believe and prove that we can solve problems that have never been solved. It brings out the best in us."</b></font></p><p align="right"><font size="+1"><b>-Bill Nye</b></font></p>',
+  '<p align="center"><font size="+1"><b>"TEST."</b></font></p><p align="right"><font size="+1"><b>-Test Test</b></font></p>',
 ];
 
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotes.length));
   var quote = quotes[randomNumber];
-  
   document.getElementById('quoteDisplay').innerHTML = quote
 }
