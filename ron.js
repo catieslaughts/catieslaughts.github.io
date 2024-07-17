b3054dc724157993fbc6d9d7b178f8246540e090
Catherine Slaughter <55809063+catieslaughts@users.noreply.github.com>
2024-07-16 19:51:43 -0500

    Update ron.js

--- ron.js
+++ ron.js
@@ -2,10 +2,15 @@ var quotes = ["quotes/billnye_oneline.txt"];
 
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotes.length));
-  var fileName = quotes[randomNumber]
+  var fileName = quotes[randomNumber];
   
-  fetch("quotes/billnye_oneline.txt").then((res) => res.text()).then((text) => {
-    var quote = text
-  }).catch((e) => console.error(e));
+  var fr = new FileReader();
+  fr.onload = function(e) {
+    // e.target.result should contain the text
+  };
+  fr.readAsText(file);
+  
+  var quote = e.target.result
+  
   document.getElementById('quoteDisplay').innerHTML = quote
 }
